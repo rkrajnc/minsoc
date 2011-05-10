94dcdcd696ebd845315f62e5d5ab3396af23bb87
rfajardo <rfajardo@1e56cc09-5d24-4cb8-85b2-5a87126a96e3>
2011-05-10 13:52:00 +0000

    Makefile does not automatic clean anymore. In Windows rm -f leads to errors and abort synthesis.
    minsoc/syn/setup.bat added. Asks for Xilinx settings32|64.bat script and completely implements MinSoC. On completion or error, it holds window open until user input ENTER. 
    
    minsoc/sim/modelsim/: *.bat holds window open until user input ENTER. 
                          run_sim.bat asks for target firmware, check if it exists before running simulator. Holds window open and output error message if not found. Does not hold for simulator.

--- sim/modelsim/run_sim.bat
+++ sim/modelsim/run_sim.bat
@@ -1,3 +1,8 @@
 @echo off
-set /p filename=Input the target firmware hex file along with its path. Ex: "..\..\sw\uart\uart-nocache.hex": 
-vsim -lib minsoc minsoc_bench -pli ../../bench/verilog/vpi/jp-io-vpi.dll +file_name=%filename%
+set /p target_firmware=Input the target firmware hex file along with its path. Ex: "..\..\sw\uart\uart-nocache.hex": 
+if EXIST %target_firmware% (
+vsim -lib minsoc minsoc_bench -pli ../../bench/verilog/vpi/jp-io-vpi.dll +file_name=%target_firmware%
+) else (
+echo %target_firmware% could not be found. 
+set /p exit=Press ENTER to close this window... 
+)
