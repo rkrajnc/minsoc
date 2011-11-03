a14fd8aaacea10674c93f6a4dbe2f1a762a717d7
rfajardo <rfajardo@1e56cc09-5d24-4cb8-85b2-5a87126a96e3>
2011-11-03 14:10:18 +0000

    ModelSim simulation, running top_module minsoc_bench_clock now, instead of minsoc_bench.

--- sim/modelsim/run_sim.bat
+++ sim/modelsim/run_sim.bat
@@ -1,7 +1,7 @@
 @echo off
 set /p target_firmware=Input the target firmware hex file along with its path. Ex: "..\..\sw\uart\uart-nocache.hex": 
 if EXIST %target_firmware% (
-vsim -lib minsoc minsoc_bench -pli ../../bench/verilog/vpi/jp-io-vpi.dll +file_name=%target_firmware%
+vsim -lib minsoc minsoc_bench_clock -pli ../../bench/verilog/vpi/jp-io-vpi.dll +file_name=%target_firmware%
 ) else (
 echo %target_firmware% could not be found. 
 set /p exit=Press ENTER to close this window... 
--- sim/modelsim/run_sim.sh
+++ sim/modelsim/run_sim.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-vsim -lib minsoc minsoc_bench -pli ../../bench/verilog/vpi/jp-io-vpi.so +file_name=$1
+vsim -lib minsoc minsoc_bench_clock -pli ../../bench/verilog/vpi/jp-io-vpi.so +file_name=$1
