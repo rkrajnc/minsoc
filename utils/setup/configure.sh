a22025cb14198a0a2f2c934ab625a543f20116f3
rfajardo <rfajardo@1e56cc09-5d24-4cb8-85b2-5a87126a96e3>
2011-11-22 10:54:44 +0000

    Updating configure.sh:     1) we don't patch the trunk version.     2) it is better to re-compile the firmwares on reconfiguration. So compiling firmwares went to configure scripts instead.
    backend/xxx/configure: compiling firmwares here now.

--- utils/setup/configure.sh
+++ utils/setup/configure.sh
@@ -16,24 +16,3 @@ execcmd "Turning off Advanced Debug System's JSP" "sed 's%\`define DBG_JSP_SUPPO
 execcmd "cd ${DIR_TO_INSTALL}/minsoc/rtl/verilog/adv_debug_sys/Software/adv_jtag_bridge/sim_lib/icarus"
 execcmd "Compiling VPI interface to connect GDB with simulation" "make"
 execcmd "cp jp-io-vpi.vpi ${DIR_TO_INSTALL}/minsoc/bench/verilog/vpi"
-
-#Patching OpenRISC Release 1 with Advanced Debug System patch for Watchpoints
-execcmd "cd ${DIR_TO_INSTALL}/minsoc/rtl/verilog/or1200/rtl/verilog"
-cecho "Patching OpenRISC for watchpoint support" 
-#patch -p0 < ${DIR_TO_INSTALL}/minsoc/rtl/verilog/adv_debug_sys/Patches/OR1200v1/or1200v1_hwbkpt.patch
-patch -p0 < ${SCRIPT_DIR}/or1200v1_hwbkpt.patch
-
-
-#Precompiling firmwares
-cecho "\nPrecompiling delivered firmwares";
-execcmd "cd ${DIR_TO_INSTALL}/minsoc/sw/utils"
-execcmd "Make utils" "make"
-
-execcmd "cd ${DIR_TO_INSTALL}/minsoc/sw/support"
-execcmd "Make support tools" "make"
-
-execcmd "cd ${DIR_TO_INSTALL}/minsoc/sw/drivers"
-execcmd "Make drivers" "make"
-
-execcmd "cd ${DIR_TO_INSTALL}/minsoc/sw/uart"
-execcmd "Make UART" "make"
